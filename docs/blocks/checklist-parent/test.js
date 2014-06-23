3d1fa17396c0434a365a9f6736a07b9cd4ddb2f8
aaccurso <accurso.alan@gmail.com>
2014-06-23 11:29:25 -0300

    Adds toggle checklist-parent tests

--- docs/blocks/checklist-parent/test.js
+++ docs/blocks/checklist-parent/test.js
@@ -6,7 +6,7 @@ describe('checklist-parent', function() {
 
   var s = '[ng-controller="Ctrl5"] ';
   var a = s+' input[type="checkbox"]';
-  var p = s+' button[checklist-model]:eq(0)';
+  var p = s+' button[ng-click="toggleCheck()"]';
 
   it('should initialize with correct values', function() {
     check(a, [0,1,0]);
@@ -36,6 +36,32 @@ describe('checklist-parent', function() {
     element(s+'button[ng-click="checkFirst()"]').click();
     check(a, [1,0,0]);
     expect(element(s+'pre').text()).toBe('[\n  1\n]');
+  });
+
+  it('should check all by clicking parent checkbox button', function() {
+    element(p).click();
+    expect(element(p).prop('class')).toContain('checked');
+    check(a, [1,1,1]);
+    expect(element(s+'pre').text()).toBe('[\n  1,\n  2,\n  3\n]');
+  });
+
+  it('should uncheck all by clicking parent checkbox button', function() {
+    element(s+'button[ng-click="checkAll()"]').click();
+    check(a, [1,1,1]);
+    element(p).click();
+    expect(element(p).prop('class')).toBe('checkbox');
+    check(a, [0,0,0]);
+    expect(element(s+'pre').text()).toBe('[]');
+  });
+
+  it('should toggle checked class of parent checkbox button when a children checkbox is unchecked', function() {
+    expect(element(p).prop('class')).toBe('checkbox');
+    element(p).click();
+    expect(element(p).prop('class')).toContain('checked');
+    check(a, [1,1,1]);
+    using(s+'label:eq(0)').input('checked').check(false);
+    check(a, [0,1,1]);
+    expect(element(p).prop('class')).toBe('checkbox');
   });
 
 });
