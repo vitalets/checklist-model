e91d430ba4f93f20d5af12de1a473ea4e14f26b2
breandr <brett.j.andrews@gmail.com>
2014-11-04 00:07:23 -0800

    Remove check for checkbox element
    For use on md-checkbox

--- checklist-model.js
+++ checklist-model.js
@@ -79,10 +79,6 @@ angular.module('checklist-model', [])
     terminal: true,
     scope: true,
     compile: function(tElement, tAttrs) {
-      if (tElement[0].tagName !== 'INPUT' || !tElement.attr('type', 'checkbox')) {
-        throw 'checklist-model should be applied to `input[type="checkbox"]`.';
-      }
-
       if (!tAttrs.checklistValue) {
         throw 'You should provide `checklist-value`.';
       }
